9b96321cc490a6beb640d98cbefd77ccc9b7976b
Benjamin Black <benblack86@gmail.com>
2013-02-02 18:01:33 -0500

    maths

---
+++ src/benblack86/java/Maths.java
@@ -0,0 +1,14 @@
+package benblack86.java;
+
+public class Maths {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println((0.1 + 0.2) + 0.3);
+		System.out.println(0.1 + (0.2 + 0.3));
+		System.out.println((0.1 + 0.2) + 0.3 == 0.1 + (0.2 + 0.3));
+	}
+
+}
